5f7179f5f637d7fa058e25e672ac26a5c9cd6ab3
KimJeonghun91 <suuuk3@naver.com>
2020-10-21 11:02:24 +0900

    init

---
+++ app/src/main/java/com/studio0610/iotfrige/MainActivity.java
@@ -0,0 +1,14 @@
+package com.studio0610.iotfrige;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
