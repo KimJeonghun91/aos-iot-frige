32a69fb331640204bf9e99861ee8009460b9a68c
KimJeonghun91 <suuuk3@naver.com>
2020-10-21 13:24:38 +0900

    ML 추가

--- app/src/main/java/com/studio0610/iotfrige/MainActivity.java
+++ app/src/main/java/com/studio0610/iotfrige/MainActivity.java
@@ -1,8 +1,24 @@
 package com.studio0610.iotfrige;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.automl.AutoMLImageLabelerLocalModel;
+import com.google.mlkit.vision.label.automl.AutoMLImageLabelerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // https://firebase.google.com/docs/ml/android/label-images-with-automl?authuser=0
+        // 파이어베이스에서 학습완료 누르면 테스트 가능.
+        AutoMLImageLabelerLocalModel localModel =
+                new AutoMLImageLabelerLocalModel.Builder()
+                        .setAssetFilePath("manifest.json")
+                        .build();
+
+        AutoMLImageLabelerOptions autoMLImageLabelerOptions =
+                new AutoMLImageLabelerOptions.Builder(localModel)
+                        .setConfidenceThreshold(0.0f)  // Evaluate your model in the Firebase console
+                        // to determine an appropriate value.
+                        .build();
+        ImageLabeler labeler = ImageLabeling.getClient(autoMLImageLabelerOptions);
+
+
+        // ** ML 대상 이미지
+        Bitmap bitmap = BitmapFactory.decodeResource( getResources(), R.drawable.flower1);
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+
+        // ** ML 시작
+        labeler.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<ImageLabel> labels) {
+                        for (ImageLabel label : labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            int index = label.getIndex();
+
+                            Log.d("label","라벨 텍스트 : "+text);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ERROR","labeler process ERROR : "+e);
+                    }
+                });
     }
 }
